d99ec24c4fc99401ab3b92d48c8766597cde2ea8
website-creator-art <57727620+website-creator-art@users.noreply.github.com>
2019-11-27 15:53:43 +0100

    Add files via upload

---
+++ js/dom.js
@@ -0,0 +1,61 @@
+// console.dir(document);
+// console.log(document.title);
+// document.title = "foooood";
+// console.log(document.images);
+// var headerTitle = document.querySelector('#header-title2');
+// headerTitle.style.color = '#f46036';
+
+
+// EVENTS
+
+// var buttonTest = document.getElementById('button-test').addEventListener('click', buttonFunction);
+
+// function buttonFunction(e){
+//     document.getElementById('title-test').textContent = 'TEST';
+// console.log(e);
+// console.log(e.target);
+// }
+
+
+// var buttonTest = document.getElementById('button-test');
+// var box = document.getElementById('box');
+// buttonTest.addEventListener('click', runEvent);
+// buttonTest.addEventListener('mousedown', runEvent);
+// buttonTest.addEventListener('mouseup', runEvent);
+
+// box.addEventListener('mouseenter', runEvent);
+// box.addEventListener('mouseleave', runEvent);
+
+// box.addEventListener('mouseover', runEvent);
+// box.addEventListener('mouseout', runEvent);
+
+// box.addEventListener('mousemove', runEvent);
+
+// var itemInput = document.querySelector('#input-test');
+// var form = document.querySelector('#form-test');
+// var form2 = document.getElementById('form-test');
+// var select = document.getElementById('select-test');
+// itemInput.addEventListener('keydown', runEvent);
+// itemInput.addEventListener('keyup', runEvent);
+// itemInput.addEventListener('keypress', runEvent);
+
+// itemInput.addEventListener('focus', runEvent);
+// itemInput.addEventListener('blur', runEvent);
+
+// itemInput.addEventListener('cut', runEvent);
+// itemInput.addEventListener('paste', runEvent);
+
+// itemInput.addEventListener('input', runEvent);
+
+// select.addEventListener('change', runEvent);
+// form.addEventListener('submit', runEvent);
+
+// function runEvent(e){
+//     e.preventDefault();
+// console.log('EVENT TYPE: '+ e.type);
+// console.log(e.target.value);
+// document.getElementById('output').innerHTML = "<h3>"+e.target.value+"</h3>"
+// select.style.backgroundColor = 'yellow';
+
+// box.style.backgroundColor = "rgb("+e.offsetX+", "+e.offsetY+", 40)";
+// }
